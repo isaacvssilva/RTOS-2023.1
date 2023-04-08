8b92cd0e893ef598a3c7529a8f776798705c65a4
isaac <isaacvinicius2121@alu.ufc.br>
2023-04-08 00:07:14 -0300

    add - blink de dois Leds usando tasks

---
+++ workspace/01-tasks-blink/Debug/source_rtos/FreeRTOS/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../source_rtos/FreeRTOS/event_groups.c \
+../source_rtos/FreeRTOS/list.c \
+../source_rtos/FreeRTOS/queue.c \
+../source_rtos/FreeRTOS/stream_buffer.c \
+../source_rtos/FreeRTOS/tasks.c \
+../source_rtos/FreeRTOS/timers.c 
+
+OBJS += \
+./source_rtos/FreeRTOS/event_groups.o \
+./source_rtos/FreeRTOS/list.o \
+./source_rtos/FreeRTOS/queue.o \
+./source_rtos/FreeRTOS/stream_buffer.o \
+./source_rtos/FreeRTOS/tasks.o \
+./source_rtos/FreeRTOS/timers.o 
+
+C_DEPS += \
+./source_rtos/FreeRTOS/event_groups.d \
+./source_rtos/FreeRTOS/list.d \
+./source_rtos/FreeRTOS/queue.d \
+./source_rtos/FreeRTOS/stream_buffer.d \
+./source_rtos/FreeRTOS/tasks.d \
+./source_rtos/FreeRTOS/timers.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source_rtos/FreeRTOS/%.o source_rtos/FreeRTOS/%.su source_rtos/FreeRTOS/%.cyclo: ../source_rtos/FreeRTOS/%.c source_rtos/FreeRTOS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I"/home/isaac/Documents/SO-TempoReal/workspace/01-tasks-blink/source_rtos/FreeRTOS/portable/GCC/ARM_CM3" -I"/home/isaac/Documents/SO-TempoReal/workspace/01-tasks-blink/source_rtos/FreeRTOS/include" -I"/home/isaac/Documents/SO-TempoReal/workspace/01-tasks-blink/source_rtos/FreeRTOS" -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-source_rtos-2f-FreeRTOS
+
+clean-source_rtos-2f-FreeRTOS:
+	-$(RM) ./source_rtos/FreeRTOS/event_groups.cyclo ./source_rtos/FreeRTOS/event_groups.d ./source_rtos/FreeRTOS/event_groups.o ./source_rtos/FreeRTOS/event_groups.su ./source_rtos/FreeRTOS/list.cyclo ./source_rtos/FreeRTOS/list.d ./source_rtos/FreeRTOS/list.o ./source_rtos/FreeRTOS/list.su ./source_rtos/FreeRTOS/queue.cyclo ./source_rtos/FreeRTOS/queue.d ./source_rtos/FreeRTOS/queue.o ./source_rtos/FreeRTOS/queue.su ./source_rtos/FreeRTOS/stream_buffer.cyclo ./source_rtos/FreeRTOS/stream_buffer.d ./source_rtos/FreeRTOS/stream_buffer.o ./source_rtos/FreeRTOS/stream_buffer.su ./source_rtos/FreeRTOS/tasks.cyclo ./source_rtos/FreeRTOS/tasks.d ./source_rtos/FreeRTOS/tasks.o ./source_rtos/FreeRTOS/tasks.su ./source_rtos/FreeRTOS/timers.cyclo ./source_rtos/FreeRTOS/timers.d ./source_rtos/FreeRTOS/timers.o ./source_rtos/FreeRTOS/timers.su
+
+.PHONY: clean-source_rtos-2f-FreeRTOS
+
